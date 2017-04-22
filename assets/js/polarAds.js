8d4fa6e1048d46e3a589b1fa653c72c4380e98c4
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-04-22 22:49:08 +0545

    ads added

--- assets/js/polarAds.js
+++ assets/js/polarAds.js
@@ -1,5 +1,20 @@
 //  Polar Ads
+var PolarAds = window.PolarAds || {};
+
+PolarAds.sideBar = function () {
+  var $container = $('.polar-ads-container');
+  $('body').prepend($container);
+  var $closeBtn = $('#polar-ads-close');
+
+  $closeBtn.click(function () {
+    $container.fadeOut(1000)
+  });
+
+};
+
 $(document).ready(function () {
+  new PolarAds.sideBar();
+
   var $adHolder = $('.oama-ad-holder');
   var oamaUrl = 'https://oama.herokuapp.com/api/v1/ads/';
   $adHolder.each(function (index, el) {
@@ -13,12 +28,27 @@ $(document).ready(function () {
         width: response.dimensions.width,
         height: response.dimensions.height
       });
-      var $link = $('<a>', {
-        href: response.url,
-        target: '_blank'
-      });
+      var openInSideBar = true;
+      var $link = '';
+      var openInNewPage = false;
+
+      if (openInSideBar) {
+        $link = $('<a>', {
+          href: response.url
+        });
+        $link.click(function () {
+          $('.polar-ads-container').fadeIn(1000);
+        })
+      } else {
+        $link = $('<a>', {
+          href: response.url,
+          target: openInNewPage ? '_blank' : ''
+        });
+      }
+
       $image.appendTo($link);
       $adHolder.html($link);
     });
+
   });
 });
